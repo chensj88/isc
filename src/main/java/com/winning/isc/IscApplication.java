175e48316e067e078878519a98c858f2e62b4069
chensj881008 <chenshijie1988@yeah.net>
2018-10-29 15:00:29 +0800

    项目初始化

---
+++ src/main/java/com/winning/isc/IscApplication.java
@@ -0,0 +1,12 @@
+package com.winning.isc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscApplication.class, args);
+    }
+}
