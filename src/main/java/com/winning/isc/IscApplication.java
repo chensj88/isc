a37da38793c94b579b57497080c9c6a6b60faa38
chensj881008 <chenshijie1988@yeah.net>
2018-10-29 16:43:40 +0800

    修改包扫描位置

--- src/main/java/com/winning/isc/IscApplication.java
+++ src/main/java/com/winning/isc/IscApplication.java
@@ -3,10 +3,11 @@ package com.winning.isc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan({"com.winning.isc.model"})
+@ComponentScan("com.winning.isc")
 @EnableTransactionManagement
 public class IscApplication {
 
