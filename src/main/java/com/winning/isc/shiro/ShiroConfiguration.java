bc1b54737162b11e662d3676bc9ac30f6188574d
Evol <798615946@qq.com>
2018-10-31 13:02:47 +0800

    整合shiro

---
+++ src/main/java/com/winning/isc/shiro/ShiroConfiguration.java
@@ -0,0 +1,91 @@
+package com.winning.isc.shiro;
+
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfiguration {
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean(name = "shiroRealm")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public WinningShiroRealm shiroRealm() {
+        WinningShiroRealm realm = new WinningShiroRealm();
+        return realm;
+    }
+
+    @Bean(name = "ehCacheManager")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public EhCacheManager ehCacheManager() {
+        EhCacheManager ehCacheManager = new EhCacheManager();
+        return ehCacheManager;
+    }
+
+    @Bean("securityManager")
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(shiroRealm());
+        securityManager.setCacheManager(ehCacheManager());//用户授权/认证信息Cache, 采用EhCache 缓存
+        return securityManager;
+    }
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Map<String, String> filterChainDefinitionManager = new LinkedHashMap<>();
+        filterChainDefinitionManager.put("/logout", "logout");
+        filterChainDefinitionManager.put("/user/**", "authc,roles[user]");
+        filterChainDefinitionManager.put("/shop/**", "authc,roles[shop]");
+        filterChainDefinitionManager.put("/admin/**", "authc,roles[admin]");
+        filterChainDefinitionManager.put("/login", "anon");//anon 可以理解为不拦截
+        filterChainDefinitionManager.put("/ajaxLogin", "anon");//anon 可以理解为不拦截
+        filterChainDefinitionManager.put("/static/**", "anon");//静态资源不拦截
+        //filterChainDefinitionManager.put("/**", "authc,roles[user]");//其他资源全部拦截
+        filterChainDefinitionManager.put("/**", "anon");
+        // filterChainDefinitionManager.put("/controller/MangerController", "anon");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionManager);
+
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setSuccessUrl("/");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    @ConditionalOnBean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator daap = new DefaultAdvisorAutoProxyCreator();
+        daap.setProxyTargetClass(true);
+        return daap;
+    }
+
+    /**
+     * 开启shiro aop注解支持
+     * 使用代理方式，所以需要开启代码支持
+     *
+     * @param securityManager
+     * @return
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
+        aasa.setSecurityManager(securityManager);
+        return aasa;
+    }
+}
--- src/main/java/com/winning/isc/shiro/WinningShiroRealm.java
+++ src/main/java/com/winning/isc/shiro/WinningShiroRealm.java
@@ -11,6 +11,8 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,16 +25,22 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class WinningShiroRealm extends AuthorizingRealm {
-
-
+    private Logger logger = LoggerFactory.getLogger(WinningShiroRealm.class);
     @Autowired
     private SysUserInfoDao sysUserInfoDao; //需要查询用户基本信息及权限的dao接口
     @Autowired
     private SysOrgExtDao sysOrgExtDao;
     @Autowired
     private PmisWebServiceClient pmisWebServiceClient;
+
+    /**
+     * 权限认证
+     * @param principals
+     * @return
+     */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        logger.info("##################执行Shiro权限认证##################");
         String username = (String) principals.getPrimaryPrincipal();// 取得用户登录名
         SimpleAuthorizationInfo auth = new SimpleAuthorizationInfo();
 	/*	try {
@@ -44,6 +52,12 @@ public class WinningShiroRealm extends AuthorizingRealm {
         return auth;
     }
 
+    /**
+     * 登录认证
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         String userid = (String) token.getPrincipal();
@@ -55,12 +69,12 @@ public class WinningShiroRealm extends AuthorizingRealm {
             throw new UnknownAccountException("该用户不存在");
         } else {
             String password = new String((char[]) token.getCredentials());
-            QueryResult result = pmisWebServiceClient.userLoginValidateByPmis(userid,password);
+            QueryResult result = pmisWebServiceClient.userLoginValidateByPmis(userid, password);
             //通过返回值中记录record中记录信息来验证登录是否错误
             int resultNum = Integer.parseInt(result.getRecords().get(0).getValues().get(0).toString());
-            if( resultNum == userInfo.getId().intValue()){
+            if (resultNum == userInfo.getId().intValue()) {
                 return new SimpleAuthenticationInfo(userInfo, password, "memberRealm");
-            }else{
+            } else {
                 throw new IncorrectCredentialsException(result.getRecords().get(0).getValues().get(2).toString());
             }
         }
