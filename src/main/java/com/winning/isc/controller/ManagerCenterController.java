e48733e46bbca5d22af7495e325b36bf821292b9
Evol <798615946@qq.com>
2018-11-05 17:22:49 +0800

    1.文本编辑器 2.文件上传

--- src/main/java/com/winning/isc/controller/ManagerCenterController.java
+++ src/main/java/com/winning/isc/controller/ManagerCenterController.java
@@ -1,11 +1,24 @@
 package com.winning.isc.controller;
 
+import com.winning.isc.base.utils.FtpUtils;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/manager")
-public class ManagerCenterController extends BaseController{
+public class ManagerCenterController extends BaseController {
     /**
      * 工具页面
      *
@@ -14,6 +27,32 @@ public class ManagerCenterController extends BaseController{
     @RequestMapping(value = "/index")
     public String index() {
         return "views/managerCenter/index";
+    }
+
+    /**
+     * 文件上传
+     *
+     * @return
+     */
+    @PostMapping(value = "/upload")
+    @ResponseBody
+    public Map upload(HttpServletRequest request, @RequestParam MultipartFile logoFile) throws IOException {
+        if (logoFile != null && !logoFile.isEmpty()) {
+            File file = null;
+            try {
+                String fileName = logoFile.getOriginalFilename();
+                String prefix="isc_";
+                file = file = File.createTempFile(prefix, fileName);
+                logoFile.transferTo(file);
+                String remote = "/isc/logo/" + fileName;
+                FtpUtils.uploadFile(remote, file);
+                file.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return resultMap;
     }
 
 }
